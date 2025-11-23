e65ce789f5d14f864e0cac7d651d4798c95730e2
Joseph Jana <mutaher.khan.skipq@gmail.com>
2025-11-23 14:02:13 +0500

    Add root URL redirect to login

--- mcm_admission/urls.py
+++ mcm_admission/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,8 +24,7 @@ urlpatterns = [
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('admissions/', include('admissions.urls')),
     path('notifications/', include('notifications.urls')),
-
-
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),  # Redirect root to login
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
